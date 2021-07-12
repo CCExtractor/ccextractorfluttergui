f35580a217f0fb7665d15862c43d324cb15e13ea
Jayesh Nirve <nirvejayesh@gmail.com>
2021-07-13 04:02:55 +0530

    use ccx from same dir as gui

--- lib/repositories/ccextractor.dart
+++ lib/repositories/ccextractor.dart
@@ -18,7 +18,7 @@ class CCExtractor {
   SettingsRepository settingsRepository = SettingsRepository();
   SettingsModel settings = SettingsModel();
   String get ccextractor {
-    return Platform.isWindows ? 'ccextractorwinfull.exe' : 'ccextractor';
+    return Platform.isWindows ? './ccextractorwinfull.exe' : 'ccextractor';
   }
 
   Future<int> extractFile(
