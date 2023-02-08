8322c41dbaa6b7d9a18590f3ed153d907520b730
Manoj <manojparasuram.sadhanala@gmail.com>
2023-02-09 02:58:24 +0530

    Update dashboard.dart

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -1,5 +1,3 @@
-import 'package:desktop_drop/desktop_drop.dart';
-import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
